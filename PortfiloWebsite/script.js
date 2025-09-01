2aebb1dda1d6ff10b1a628563859bc31c79580a5
KyleAngeles <kyle.angeles@dcmail.ca>
2025-09-01 14:48:21 -0400

    adding the sliding features

--- PortfiloWebsite/script.js
+++ PortfiloWebsite/script.js
@@ -27,3 +27,27 @@ window.addEventListener("load", () => {
         }
     }
 });
+
+/* FUNCTION FOR THE SLIDING FEATURE CARDS */
+const wrapper = document.querySelector('.services-wrapper');
+const nextBtn = document.querySelector('.slide-btn.next');
+const prevBtn = document.querySelector('.slide-btn.prev');
+
+
+let currentIndex = 0;
+const cardWidth = 320;
+const totalCards = document.querySelectorAll('.services-wrapper > div').length;
+
+nextBtn.addEventListener('click', () =>{
+    if (currentIndex < totalCards - 1 ) {
+        currentIndex++;
+        wrapper.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+    }
+});
+
+prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+         wrapper.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+    }
+})
